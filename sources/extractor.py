3dc661ed98fee504ece120be5dfe6fabccca6f54
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-13 01:52:11 +0300

    implemented feature to extract data from Oracle MySQL

--- sources/extractor.py
+++ sources/extractor.py
@@ -24,18 +24,18 @@ if __name__ == '__main__':
     c_bn.fn_load_configuration()
     # instantiate Command Line Arguments class
     c_clam = CommandLineArgumentsManagement()
-    parameters_in = c_clam.parse_arguments(c_bn.cfg_dtls['input_options']['data_extractor'])
+    parameters_in = c_clam.parse_arguments(c_bn.cfg_dtls['input_options']['db_extractor'])
     # checking inputs, if anything is invalid an exit(1) will take place
     c_bn.fn_check_inputs(parameters_in, current_script_name)
     # instantiate Logger class
     c_ln = LoggingNeeds()
     # initiate logger
-    c_ln.initiate_logger(parameters_in.output_log_file, 'data_extractor')
+    c_ln.initiate_logger(parameters_in.output_log_file, 'db_extractor')
     # define global timer to use
-    t = Timer('data_extractor', text='Time spent is {seconds} ', logger=c_ln.logger.debug)
+    t = Timer('db_extractor', text='Time spent is {seconds} ', logger=c_ln.logger.debug)
     # reflect title and input parameters given values in the log
     c_clam.listing_parameter_values(c_ln.logger, t, 'Data extractor',
-                                    c_bn.cfg_dtls['input_options']['data_extractor'],
+                                    c_bn.cfg_dtls['input_options']['db_extractor'],
                                     parameters_in)
     # loading extracting sequence details
     t.start()
@@ -65,6 +65,8 @@ if __name__ == '__main__':
     c_ph = ParameterHandling()
     # instantiate Data Manipulator class, useful to manipulate data frames
     c_dm = DataManipulator()
+    # instantiate Database Talker class
+    c_dbtkr = DatabaseTalker()
     # cycling through the configurations
     for current_extracting_sequence in extracting_sequences:
         t.start()
@@ -75,16 +77,17 @@ if __name__ == '__main__':
             'grp': current_extracting_sequence['server-group'],
             'lyr': current_extracting_sequence['server-layer']
         }
+        usr_lbl = current_extracting_sequence['account-label']
         # variable for source server details
         src_server = source_systems[srv['vdr']][srv['typ']]['Server'][srv['grp']][srv['lyr']]
         # variable with credentials for source server
-        usr_dtl = configured_secrets[srv['vdr']][srv['typ']][srv['grp']][srv['lyr']]['Default']
+        usr_dtl = configured_secrets[srv['vdr']][srv['typ']][srv['grp']][srv['lyr']][usr_lbl]
         c_ln.logger.info('Preparing connection details has been completed')
         t.stop()
-        # instantiate Database Talker class
-        c_dbtkr = DatabaseTalker()
-        if srv['vdr'] == 'SAP' and srv['typ'] == 'HANA':
-            c_dbtkr.connect_to_sap_hana(c_ln.logger, t, {
+        server_vendor_and_type = srv['vdr'] + ' ' + srv['typ']
+        if server_vendor_and_type in ('Oracle MySQL', 'SAP HANA'):
+            c_dbtkr.connect_to_database(c_ln.logger, t, {
+                'server-vendor-and-type': server_vendor_and_type,
                 'server-layer': srv['lyr'],
                 'ServerName': src_server['ServerName'],
                 'ServerPort': int(src_server['ServerPort']),
@@ -92,40 +95,51 @@ if __name__ == '__main__':
                 'Name': usr_dtl['Name'],
                 'Password': usr_dtl['Password'],
              })
-        # instantiate DB connection handler
-        cursor = c_dbtkr.conn.cursor()
-        for current_query in current_extracting_sequence['queries']:
-            t.start()
-            initial_query = c_bn.fn_open_file_and_get_content(
-                current_query['input-query-file'], 'raw')
-            c_ln.logger.info('Generic query is: '
-                             + c_bn.fn_multi_line_string_to_single_line(initial_query))
-            t.stop()
-            for current_session in current_query['sessions']:
-                # ensure all potential query parameters are properly injected
-                query_to_run = c_ph.handle_query(c_ln.logger, t, current_session, initial_query)
-                # actual execution of the query
-                cursor = c_dbtkr.execute_query(c_ln.logger, t, cursor, query_to_run)
-                # bringing the information from server (data transfer)
-                result_set = c_dbtkr.fetch_executed_query(c_ln.logger, t, cursor)
-                # detecting the column named from result set
-                columns_name = c_dbtkr.determine_column_names(c_ln.logger, t, cursor)
-                c_ln.logger.info('Free DB result-set started')
-                cursor.close()
-                c_ln.logger.info('Free DB result-set completed')
+        c_ln.logger.debug('Connection attempt done')
+        if c_dbtkr.conn is not None:
+            # instantiate DB connection handler
+            cursor = c_dbtkr.conn.cursor()
+            for current_query in current_extracting_sequence['queries']:
+                t.start()
+                initial_query = c_bn.fn_open_file_and_get_content(
+                    current_query['input-query-file'], 'raw')
+                c_ln.logger.info('Generic query is: '
+                                 + c_bn.fn_multi_line_string_to_single_line(initial_query))
                 t.stop()
-                # put result set into a data frame
-                result_data_frame = c_dbtkr.result_set_to_data_frame(c_ln.logger, t,
-                                                                     columns_name, result_set)
-                #
-                c_dm.fn_store_data_frame_to_file(c_ln.logger, t, result_data_frame,
-                                                 current_session['output-csv-file'],
-                                                 current_session['output-csv-separator'])
-        t.start()
-        c_ln.logger.info('Closing DB connection')
-        c_dbtkr.conn.close()
-        c_ln.logger.info('Closing DB completed')
-        t.stop()
+                for current_session in current_query['sessions']:
+                    # ensure all potential query parameters are properly injected
+                    query_to_run = c_ph.handle_query(c_ln.logger, t, current_session, initial_query)
+                    # actual execution of the query
+                    cursor = c_dbtkr.execute_query(c_ln.logger, t, cursor, query_to_run)
+                    # bringing the information from server (data transfer)
+                    result_set = c_dbtkr.fetch_executed_query(c_ln.logger, t, cursor)
+                    # detecting the column named from result set
+                    stats = {
+                        'columns': cursor.column_names,
+                        'rows_count': cursor.rowcount,
+                    }
+                    t.start()
+                    c_ln.logger.info('Free DB result-set started')
+                    cursor.close()
+                    c_ln.logger.info('Free DB result-set completed')
+                    t.stop()
+                    if stats['rows_count'] > 0:
+                        # put result set into a data frame
+                        result_data_frame = c_dbtkr.result_set_to_data_frame(c_ln.logger, t,
+                                                                             stats['columns'],
+                                                                             result_set)
+                        # store data frame to a specified output file
+                        c_dm.fn_store_data_frame_to_file(c_ln.logger, t, result_data_frame,
+                                                         current_session['output-csv-file'],
+                                                         current_session['output-csv-separator'])
+                        c_bn.fn_store_file_statistics(c_ln.logger, t,
+                                                      current_session['output-csv-file'],
+                                                      'Output file name')
+            t.start()
+            c_ln.logger.info('Closing DB connection')
+            c_dbtkr.conn.close()
+            c_ln.logger.info('Closing DB completed')
+            t.stop()
     # just final message
     c_bn.fn_final_message(c_ln.logger, parameters_in.output_log_file,
-                          t.timers.total('data_extractor'))
+                          t.timers.total('db_extractor'))
