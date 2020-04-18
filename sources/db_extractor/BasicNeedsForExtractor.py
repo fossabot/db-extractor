0c3af14f8964eead8646a785548d28a1b54cdd5e
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-18 03:16:08 +0300

    started to implement extensive JSON validation for all 3 such involved files: Extraction Sequence, Source systems and User Secrets --- first 2 covered so far

--- sources/db_extractor/BasicNeedsForExtractor.py
+++ sources/db_extractor/BasicNeedsForExtractor.py
@@ -24,18 +24,15 @@ class BasicNeedsForExtractor:
                                              'file', 'extracting sequence file')
 
     @staticmethod
-    def fn_set_extract_behaviour(in_session):
-        default_value = 'skip-if-output-file-exists'
-        allowed_values = [
-            'skip-if-output-file-exists',
-            'overwrite-if-output-file-exists',
-        ]
-        if ('extract-behaviour' not in in_session) \
-                or (in_session['extract-behaviour'] not in allowed_values):
-            value_to_return = default_value
-        else:
-            value_to_return = in_session['extract-behaviour']
-        return value_to_return
+    def fn_validate_mandatory_properties(local_logger, who, properties_parent, list_properties):
+        is_valid = True
+        for current_property in list_properties:
+            if current_property not in properties_parent:
+                local_logger.error(who + ' does not contain '
+                                   + f'"{current_property}" which is mandatory, '
+                                   + 'therefore extraction sequence will be ignored')
+                is_valid = False
+        return is_valid
 
     @staticmethod
     def fn_is_extraction_necessary(local_logger, relevant_details):
@@ -57,3 +54,86 @@ class BasicNeedsForExtractor:
             extraction_is_necessary = True
             local_logger.debug('Database extraction has to be performed')
         return extraction_is_necessary
+
+    @staticmethod
+    def fn_set_extract_behaviour(in_session):
+        default_value = 'skip-if-output-file-exists'
+        allowed_values = [
+            'skip-if-output-file-exists',
+            'overwrite-if-output-file-exists',
+        ]
+        if ('extract-behaviour' not in in_session) \
+                or (in_session['extract-behaviour'] not in allowed_values):
+            value_to_return = default_value
+        else:
+            value_to_return = in_session['extract-behaviour']
+        return value_to_return
+
+    @staticmethod
+    def validate_extraction_sequence_file(local_logger, in_extract_sequence):
+        is_valid = True
+        if str(type(in_extract_sequence)) != "<class 'list'>":
+            local_logger.error('Extraction sequence file name is not a LIST, therefore cannot be used')
+            is_valid = False
+        elif len(in_extract_sequence) < 1:
+            local_logger.error('Extraction sequence file name is a LIST '
+                               + 'but does not contain at least 1 extraction sequence, '
+                               + 'therefore cannot be used')
+            is_valid = False
+        return is_valid
+
+    def validate_extraction_sequence(self, local_logger, in_extraction_sequence):
+        mandatory_props_e = [
+            'server-vendor',
+            'server-type',
+            'server-group',
+            'server-layer',
+            'account-label',
+            'queries',
+        ]
+        is_valid = self.fn_validate_mandatory_properties(local_logger, 'Extraction Sequence',
+                                                         in_extraction_sequence,
+                                                         mandatory_props_e)
+        return is_valid
+
+    def validate_source_systems_file(self, local_logger, in_sequence, in_source_systems):
+        crt_item = in_sequence['server-vendor']
+        can_proceed_s = self.fn_validate_mandatory_properties(local_logger, 'Source Systems',
+                                                              in_source_systems, [crt_item])
+        can_proceed_s1 = False
+        if can_proceed_s:
+            item_checked = in_source_systems[crt_item]
+            crt_item1 = in_sequence['server-type']
+            can_proceed_s1 = self.fn_validate_mandatory_properties(local_logger, 'Source Systems "'
+                                                                   + crt_item + '"',
+                                                                   item_checked, [crt_item1])
+        can_proceed_s2 = False
+        if can_proceed_s1:
+            crt_item2 = in_sequence['server-group']
+            item_checked = in_source_systems[crt_item][crt_item1]['Server']
+            can_proceed_s2 = self.fn_validate_mandatory_properties(local_logger, 'Source Systems "'
+                                                                   + crt_item
+                                                                   + '", "' + crt_item1
+                                                                   + '", "Server" ',
+                                                                   item_checked, [crt_item2])
+        can_proceed_s3 = False
+        if can_proceed_s2:
+            crt_item3 = in_sequence['server-layer']
+            item_checked = in_source_systems[crt_item][crt_item1]['Server'][crt_item2]
+            can_proceed_s3 = self.fn_validate_mandatory_properties(local_logger, 'Source Systems "'
+                                                                   + crt_item
+                                                                   + '", "' + crt_item1
+                                                                   + '", "' + crt_item2
+                                                                   + '", "Server" ',
+                                                                   item_checked, [crt_item3])
+        return can_proceed_s and can_proceed_s1 and can_proceed_s2 and can_proceed_s3
+
+    def validate_source_system(self, local_logger, str_source_system, in_source_system):
+        mandatory_properties = [
+            'ServerName',
+            'ServerPort',
+        ]
+        is_valid = self.fn_validate_mandatory_properties(local_logger,
+                                                         'Source System ' + str_source_system,
+                                                         in_source_system, mandatory_properties)
+        return is_valid
--- sources/extractor.py
+++ sources/extractor.py
@@ -28,46 +28,50 @@ if __name__ == '__main__':
     # instantiate Command Line Arguments class
     c_clam = CommandLineArgumentsManagement()
     # get command line parameter values
-    parameters_in = c_clam.parse_arguments(c_bn.cfg_dtls['input_options']['db_extractor'])
+    in_params = c_clam.parse_arguments(c_bn.cfg_dtls['input_options']['db_extractor'])
     # checking inputs, if anything is invalid an exit(1) will take place
-    c_bn.fn_check_inputs(parameters_in)
+    c_bn.fn_check_inputs(in_params)
     # instantiate Extractor Specific Needs class
     c_bnfe = BasicNeedsForExtractor()
     # checking inputs, if anything is invalid an exit(1) will take place
-    c_bnfe.fn_check_inputs_specific(parameters_in)
+    c_bnfe.fn_check_inputs_specific(in_params)
     # instantiate Logger class
     c_ln = LoggingNeeds()
     # initiate logger
-    c_ln.initiate_logger(parameters_in.output_log_file, 'db_extractor')
+    c_ln.initiate_logger(in_params.output_log_file, 'db_extractor')
     # define global timer to use
     t = Timer('db_extractor', text='Time spent is {seconds} ', logger=c_ln.logger.debug)
     # reflect title and input parameters given values in the log
     c_clam.listing_parameter_values(c_ln.logger, t, 'Data extractor',
                                     c_bn.cfg_dtls['input_options']['db_extractor'],
-                                    parameters_in)
+                                    in_params)
     # loading extracting sequence details
     t.start()
     extracting_sequences = c_bn.fn_open_file_and_get_content(
-        parameters_in.input_extracting_sequence_file, 'json')
-    c_ln.logger.info('Configuration file name with extracting sequence(es) has been loaded')
+        in_params.input_extracting_sequence_file, 'json')
+    c_ln.logger.info('Extraction sequence file name with extracting sequence(es) has been loaded')
     t.stop()
-    c_bn.fn_store_file_statistics(c_ln.logger, t, parameters_in.input_extracting_sequence_file,
+    c_bn.fn_store_file_statistics(c_ln.logger, t, in_params.input_extracting_sequence_file,
                                   'Configuration file name with extracting sequence(es)')
+    # validation of the extraction sequence file
+    file_is_valid = c_bnfe.validate_extraction_sequence_file(c_ln.logger, extracting_sequences)
+    if not file_is_valid:
+        exit(1)
     # get the source system details from provided file
     t.start()
     source_systems = c_bn.fn_open_file_and_get_content(
-        parameters_in.input_source_system_file, 'json')['Systems']
+        in_params.input_source_system_file, 'json')['Systems']
     c_ln.logger.info('Source Systems file name has been loaded')
     t.stop()
-    c_bn.fn_store_file_statistics(c_ln.logger, t, parameters_in.input_source_system_file,
+    c_bn.fn_store_file_statistics(c_ln.logger, t, in_params.input_source_system_file,
                                   'SourceSystems file name')
     # get the source system details from provided file
     t.start()
     configured_secrets = c_bn.fn_open_file_and_get_content(
-        parameters_in.input_credentials_file, 'json')['Credentials']
+        in_params.input_credentials_file, 'json')['Credentials']
     c_ln.logger.info('Configuration file name with credentials has been loaded')
     t.stop()
-    c_bn.fn_store_file_statistics(c_ln.logger, t, parameters_in.input_source_system_file,
+    c_bn.fn_store_file_statistics(c_ln.logger, t, in_params.input_source_system_file,
                                   'SourceSystems file name')
     # instantiate Parameter Handling class
     c_ph = ParameterHandling()
@@ -76,100 +80,117 @@ if __name__ == '__main__':
     # instantiate Database Talker class
     c_dbtkr = DatabaseTalker()
     # cycling through the configurations
-    for current_extracting_sequence in extracting_sequences:
+    int_extracting_sequence = 1
+    for crt_sequence in extracting_sequences:
         t.start()
-        # just few values that's going to be used a lot
-        srv = {
-            'vdr': current_extracting_sequence['server-vendor'],
-            'typ': current_extracting_sequence['server-type'],
-            'grp': current_extracting_sequence['server-group'],
-            'lyr': current_extracting_sequence['server-layer']
-        }
-        usr_lbl = current_extracting_sequence['account-label']
-        # variable for source server details
-        src_server = source_systems[srv['vdr']][srv['typ']]['Server'][srv['grp']][srv['lyr']]
-        # variable with credentials for source server
-        usr_dtl = configured_secrets[srv['vdr']][srv['typ']][srv['grp']][srv['lyr']][usr_lbl]
-        c_ln.logger.info('Preparing connection details has been completed')
+        can_proceed_e = c_bnfe.validate_extraction_sequence(c_ln.logger, crt_sequence)
+        can_proceed_s = c_bnfe.validate_source_systems_file(c_ln.logger, crt_sequence,
+                                                            source_systems)
+        can_proceed_ss = False
+        if can_proceed_e:
+            # just few values that's going to be used a lot
+            srv = {
+                'vdr': crt_sequence['server-vendor'],
+                'typ': crt_sequence['server-type'],
+                'grp': crt_sequence['server-group'],
+                'lyr': crt_sequence['server-layer']
+            }
+            # variable for source server details
+            src_server = source_systems[srv['vdr']][srv['typ']]['Server'][srv['grp']][srv['lyr']]
+            str_ss = '"' + '", "'.join(srv.values()) + '"'
+            can_proceed_ss = c_bnfe.validate_source_system(c_ln.logger, str_ss, src_server)
+        c_ln.logger.info('Validation of the 3 JSON files involved '
+                         + '(Extraction Sequence, Source systems and User Secrets) '
+                         + 'has been completed')
         t.stop()
-        server_vendor_and_type = srv['vdr'] + ' ' + srv['typ']
-        if server_vendor_and_type in ('Oracle MySQL', 'SAP HANA'):
-            c_dbtkr.connect_to_database(c_ln.logger, t, {
-                'server-vendor-and-type': server_vendor_and_type,
-                'server-layer': srv['lyr'],
-                'ServerName': src_server['ServerName'],
-                'ServerPort': int(src_server['ServerPort']),
-                'Username': usr_dtl['Username'],
-                'Name': usr_dtl['Name'],
-                'Password': usr_dtl['Password'],
-            })
-        c_ln.logger.debug('Connection attempt done')
-        if c_dbtkr.conn is not None:
-            # instantiate DB connection handler
-            cursor = c_dbtkr.conn.cursor()
-            for current_query in current_extracting_sequence['queries']:
-                t.start()
-                initial_query = c_bn.fn_open_file_and_get_content(
-                    current_query['input-query-file'], 'raw')
-                c_ln.logger.info('Generic query is: '
-                                 + c_bn.fn_multi_line_string_to_single_line(initial_query))
-                t.stop()
-                for current_session in current_query['sessions']:
-                    current_session['output-csv-file'] = \
-                        c_ph.special_case_string(c_ln.logger, current_session['output-csv-file'])
-                    extract_behaviour = c_bnfe.fn_set_extract_behaviour(current_session)
-                    extraction_required = c_bnfe.fn_is_extraction_necessary(c_ln.logger, {
-                        'extract-behaviour': extract_behaviour,
-                        'output-csv-file': current_session['output-csv-file'],
-                    })
-                    if extraction_required:
-                        # get query parameters into a tuple
-                        tuple_parameters = c_ph.handle_query_parameters(c_ln.logger,
-                                                                        current_session)
-                        # measure expected number of parameters
-                        parameters_expected = initial_query.count('%s')
-                        # simulate final query to log (useful for debugging purposes)
-                        query_to_run = c_ph.simulate_final_query(c_ln.logger, t, initial_query,
-                                                                 parameters_expected,
-                                                                 tuple_parameters)
-                        c_ln.logger.info('Query with parameters interpreted is: '
-                                         + c_bn.fn_multi_line_string_to_single_line(query_to_run))
-                        # actual execution of the query
-                        cursor = c_dbtkr.execute_query(c_ln.logger, t, cursor, initial_query,
-                                                       parameters_expected, tuple_parameters)
-                        # bringing the information from server (data transfer)
-                        result_set = c_dbtkr.fetch_executed_query(c_ln.logger, t, cursor)
-                        # detecting the column named from result set
-                        stats = {
-                            'columns': c_dbtkr.get_column_names(c_ln.logger, t, cursor),
-                            'rows_count': cursor.rowcount,
-                        }
-                        t.start()
-                        c_ln.logger.info('Free DB result-set started')
-                        cursor.close()
-                        c_ln.logger.info('Free DB result-set completed')
-                        t.stop()
-                        if stats['rows_count'] > 0:
-                            # put result set into a data frame
-                            result_data_frame = c_dbtkr.result_set_to_data_frame(c_ln.logger, t,
-                                                                                 stats['columns'],
-                                                                                 result_set)
-                            rdf = c_dbtkr.append_additional_columns_to_data_frame(c_ln.logger, t,
-                                                                                  result_data_frame,
-                                                                                  current_session)
-                            # store data frame to a specified output file
-                            c_dm.fn_store_data_frame_to_file(c_ln.logger, t, rdf,
-                                                             current_session['output-csv-file'],
-                                                             current_session['output-csv-separator']
-                                                             )
-                            c_bn.fn_store_file_statistics(c_ln.logger, t,
-                                                          current_session['output-csv-file'],
-                                                          'Output file name')
+        if can_proceed_e and can_proceed_s and can_proceed_ss:
             t.start()
-            c_ln.logger.info('Closing DB connection')
-            c_dbtkr.conn.close()
-            c_ln.logger.info('Closing DB completed')
+            usr_lbl = crt_sequence['account-label']
+            # variable with credentials for source server
+            usr_dtl = configured_secrets[srv['vdr']][srv['typ']][srv['grp']][srv['lyr']][usr_lbl]
+            server_vendor_and_type = srv['vdr'] + ' ' + srv['typ']
+            c_ln.logger.info('Preparing connection details has been completed')
             t.stop()
+            if server_vendor_and_type in ('Oracle MySQL', 'SAP HANA'):
+                c_dbtkr.connect_to_database(c_ln.logger, t, {
+                    'server-vendor-and-type': server_vendor_and_type,
+                    'server-layer': srv['lyr'],
+                    'ServerName': src_server['ServerName'],
+                    'ServerPort': int(src_server['ServerPort']),
+                    'Username': usr_dtl['Username'],
+                    'Name': usr_dtl['Name'],
+                    'Password': usr_dtl['Password'],
+                })
+            c_ln.logger.debug('Connection attempt done')
+            if c_dbtkr.conn is not None:
+                # instantiate DB connection handler
+                cursor = c_dbtkr.conn.cursor()
+                for crt_query in crt_sequence['queries']:
+                    t.start()
+                    initial_query = c_bn.fn_open_file_and_get_content(
+                        crt_query['input-query-file'], 'raw')
+                    c_ln.logger.info('Generic query is: '
+                                     + c_bn.fn_multi_line_string_to_single_line(initial_query))
+                    t.stop()
+                    for crt_session in crt_query['sessions']:
+                        crt_session['output-csv-file'] = \
+                            c_ph.special_case_string(c_ln.logger,
+                                                     crt_session['output-csv-file'])
+                        extract_behaviour = c_bnfe.fn_set_extract_behaviour(crt_session)
+                        extraction_required = c_bnfe.fn_is_extraction_necessary(c_ln.logger, {
+                            'extract-behaviour': extract_behaviour,
+                            'output-csv-file': crt_session['output-csv-file'],
+                        })
+                        if extraction_required:
+                            # get query parameters into a tuple
+                            tuple_parameters = c_ph.handle_query_parameters(c_ln.logger,
+                                                                            crt_session)
+                            # measure expected number of parameters
+                            parameters_expected = initial_query.count('%s')
+                            # simulate final query to log (useful for debugging purposes)
+                            query = c_ph.simulate_final_query(c_ln.logger, t, initial_query,
+                                                              parameters_expected,
+                                                              tuple_parameters)
+                            c_ln.logger.info('Query with parameters interpreted is: '
+                                             + c_bn.fn_multi_line_string_to_single_line(query))
+                            # actual execution of the query
+                            cursor = c_dbtkr.execute_query(c_ln.logger, t, cursor, initial_query,
+                                                           parameters_expected, tuple_parameters)
+                            # bringing the information from server (data transfer)
+                            result_set = c_dbtkr.fetch_executed_query(c_ln.logger, t, cursor)
+                            # detecting the column named from result set
+                            stats = {
+                                'columns': c_dbtkr.get_column_names(c_ln.logger, t, cursor),
+                                'rows_count': cursor.rowcount,
+                            }
+                            t.start()
+                            c_ln.logger.info('Free DB result-set started')
+                            cursor.close()
+                            c_ln.logger.info('Free DB result-set completed')
+                            t.stop()
+                            if stats['rows_count'] > 0:
+                                # put result set into a data frame
+                                result_df = c_dbtkr.result_set_to_data_frame(c_ln.logger, t,
+                                                                             stats['columns'],
+                                                                             result_set)
+                                rdf = c_dbtkr.append_additional_columns_to_data_frame(c_ln.logger,
+                                                                                      t,
+                                                                                      result_df,
+                                                                                      crt_session)
+                                # store data frame to a specified output file
+                                c_dm.fn_store_data_frame_to_file(c_ln.logger, t, rdf,
+                                                                 crt_session['output-csv-file'],
+                                                                 crt_session['output-csv-separator']
+                                                                 )
+                                c_bn.fn_store_file_statistics(c_ln.logger, t,
+                                                              crt_session['output-csv-file'],
+                                                              'Output file name')
+                t.start()
+                c_ln.logger.info('Closing DB connection')
+                c_dbtkr.conn.close()
+                c_ln.logger.info('Closing DB completed')
+                t.stop()
+            int_extracting_sequence += 1
     # just final message
-    c_bn.fn_final_message(c_ln.logger, parameters_in.output_log_file,
+    c_bn.fn_final_message(c_ln.logger, in_params.output_log_file,
                           t.timers.total('db_extractor'))
