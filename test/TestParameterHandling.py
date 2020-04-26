444cc165485c7585968b7d9a0e747b10d132fc36
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-26 20:10:04 +0300

    adjusted one of the tests

--- test/TestParameterHandling.py
+++ test/TestParameterHandling.py
@@ -25,13 +25,16 @@ class TestParameterHandling(unittest.TestCase):
         # parse through all pair of values and run the test
         ph = ParameterHandling()
         for current_pair in pair_values:
-            reference_date = datetime.strptime(current_pair['reference_date'], '%Y-%m-%d')
+            reference_format = '%Y-%m-%d'
+            if 'reference_format' in current_pair:
+                reference_format = current_pair['reference_format']
+            reference_date = datetime.strptime(current_pair['reference_value'], reference_format)
             expression_parts = current_pair['expression'].split('_')
             value_to_assert = ph.interpret_known_expression(reference_date, expression_parts, 1)
-            self.assertEqual(value_to_assert, current_pair['expected'],
-                             'Provided value was "' + current_pair['reference_date']
+            self.assertEqual(value_to_assert, current_pair['expected_value'],
+                             'Provided value was "' + current_pair['reference_value']
                              + '", Expression was "' + current_pair['expression'] + '" '
-                             + '", Expected was "' + current_pair['expected'] + '" '
+                             + '", Expected was "' + current_pair['expected_value'] + '" '
                              + 'but received was "' + value_to_assert + '"...')
 
 
