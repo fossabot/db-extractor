8d007798984c2955511ebf6215a3673d1e49e7ee
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-29 22:42:43 +0300

    intermediary solution for multiple file format supported

--- sources/common/DataManipulator.py
+++ sources/common/DataManipulator.py
@@ -263,18 +263,52 @@ class DataManipulator:
         timmer.stop()
         return combined_csv
 
-    def fn_store_data_frame_to_file(self, local_logger, timmer, input_data_frame,
-                                    input_file_details):
+    def fn_store_data_frame_to_file(self, local_logger, timmer, in_data_frame, in_file_details):
         timmer.start()
-        if input_file_details['format'] == 'csv':
-            input_data_frame.to_csv(path_or_buf=input_file_details['name'],
-                                    sep=input_file_details['field-delimiter'],
-                                    header=True,
-                                    index=False,
-                                    encoding='utf-8')
-        local_logger.info(self.lcl.gettext( \
-            'Pandas Data Frame has just been saved to file "{file_name}", '
-            + 'considering {file_type} as file type') \
-                          .replace('{file_name}', input_file_details['name']) \
-                          .replace('{file_type}', input_file_details['format']))
+        is_file_saved = False
+        given_format = self.fn_store_data_frame_to_file_validation(local_logger, in_data_frame,
+                                                                   in_file_details)
+        if given_format == 'csv':
+            in_data_frame.to_csv(path_or_buf=in_file_details['name'],
+                                 sep=in_file_details['field-delimiter'],
+                                 header=True,
+                                 index=False,
+                                 encoding='utf-8')
+            is_file_saved = True
+        elif given_format == 'excel':
+            in_data_frame.to_excel(excel_writer=in_file_details['name'],
+                                   engine='xlsxwriter',
+                                   freeze_panes=(1, 1),
+                                   verbose=True)
+            is_file_saved = True
+        elif given_format == 'pickle':
+            if 'compression' not in in_file_details:
+                in_file_details['compression'] = 'gzip'
+            in_data_frame.to_pickle(path=in_file_details['name'],
+                                    compression=in_file_details['compression'])
+            is_file_saved = True
+        if is_file_saved:
+            local_logger.info(self.lcl.gettext( \
+                'Pandas Data Frame has just been saved to file "{file_name}", '
+                + 'considering {file_type} as file type') \
+                              .replace('{file_name}', in_file_details['name']) \
+                              .replace('{file_type}', in_file_details['format']))
         timmer.stop()
+
+    def fn_store_data_frame_to_file_validation(self, local_logger, in_data_frame, in_file_details):
+        implemented_file_formats = ['csv', 'excel', 'pickle']
+        are_settings_ok = False
+        if 'format' in in_file_details:
+            given_format = in_file_details['format'].lower()
+            if given_format not in implemented_file_formats:
+                local_logger.error(self.lcl.gettext('File "format" attribute has a value of "{format_value}" which is not among currently implemented values ("{implemented_file_formats}", therefore file saving is not possible').replace('{format_value}', given_format).replace('{implemented_file_formats}', '", "'.join(implemented_file_formats)))
+            elif given_format == 'csv':
+                if 'field-delimiter' in in_file_details:
+                    are_settings_ok = given_format
+                else:
+                    local_logger.error(self.lcl.gettext('File format is CSV and that required an "field-delimiter" attribute with value, and that is missing, therfore file saving is not going to be performed'))
+            elif given_format in ('excel', 'pickle'):
+                are_settings_ok = given_format
+        else:
+            local_logger.error(self.lcl.gettext('File "format" attribute is mandatory in the file setting, but missing, therefore file saving is not possible'))
+        return are_settings_ok
