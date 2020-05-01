0a179df2eb81e74d283285019f11f83d98433b41
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-05-02 02:28:27 +0300

    code optimization and prepared for further extension

--- sources/common/DataInputOutput.py
+++ sources/common/DataInputOutput.py
@@ -17,6 +17,21 @@ class DataInputOutput:
         lang_folder = os.path.join(os.path.dirname(__file__), current_script + '_Locale')
         self.lcl = gettext.translation(current_script, lang_folder, languages=[default_language])
 
+    def fn_file_save_error_logger(self, local_logger, in_file_details, error_details):
+        if error_details is None:
+            local_logger.info(self.lcl.gettext(
+                'Pandas Data Frame has just been saved to file "{file_name}", '
+                + 'considering {file_type} as file type')
+                              .replace('{file_name}', in_file_details['name'])
+                              .replace('{file_type}', in_file_details['format']))
+        else:
+            local_logger.error(
+                self.lcl.gettext(
+                    'Error encountered on saving Pandas Data Frame '
+                    + 'into a {file_type} file type (see below)')
+                    .replace('{file_type}', in_file_details['format'].upper()))
+            local_logger.error(error_details)
+
     def fn_load_file_list_to_data_frame(self, local_logger, timmer, file_list, csv_delimiter):
         timmer.start()
         out_data_frame = pd.concat([pd.read_csv(filepath_or_buffer=current_file,
@@ -33,59 +48,60 @@ class DataInputOutput:
         timmer.stop()
         return out_data_frame
 
-    @staticmethod
-    def fn_save_data_frame_to_csv(in_data_frame, in_file_details):
-        if 'field-delimiter' not in in_file_details:
-            in_file_details['field-delimiter'] = os.pathsep
-        in_data_frame.to_csv(path_or_buf=in_file_details['name'],
-                             sep=in_file_details['field-delimiter'],
-                             header=True,
-                             index=False,
-                             encoding='utf-8')
+    def fn_save_data_frame_to_csv(self, local_logger, in_data_frame, in_file_details):
+        if in_file_details['format'].lower() == 'csv':
+            if 'field-delimiter' not in in_file_details:
+                in_file_details['field-delimiter'] = os.pathsep
+            try:
+                in_data_frame.to_csv(path_or_buf=in_file_details['name'],
+                                     sep=in_file_details['field-delimiter'],
+                                     header=True,
+                                     index=False,
+                                     encoding='utf-8')
+                self.fn_file_save_error_logger(local_logger, in_file_details, None)
+            except Exception as err:
+                self.fn_file_save_error_logger(local_logger, in_file_details, err)
 
-    @staticmethod
-    def fn_save_data_frame_to_pickle(in_data_frame, in_file_details):
-        if 'compression' not in in_file_details:
-            in_file_details['compression'] = 'gzip'
-        in_data_frame.to_pickle(path=in_file_details['name'],
-                                compression=in_file_details['compression'])
+    def fn_save_data_frame_to_excel(self, local_logger, in_data_frame, in_file_details):
+        if in_file_details['format'].lower() == 'excel':
+            if 'compression' not in in_file_details:
+                in_file_details['compression'] = 'gzip'
+            try:
+                in_data_frame.to_pickle(path=in_file_details['name'],
+                                        compression=in_file_details['compression'])
+                self.fn_file_save_error_logger(local_logger, in_file_details, None)
+            except Exception as err:
+                self.fn_file_save_error_logger(local_logger, in_file_details, err)
+
+    def fn_save_data_frame_to_pickle(self, local_logger, in_data_frame, in_file_details):
+        if in_file_details['format'].lower() == 'pickle':
+            if 'compression' not in in_file_details:
+                in_file_details['compression'] = 'gzip'
+            try:
+                in_data_frame.to_pickle(path=in_file_details['name'],
+                                        compression=in_file_details['compression'])
+                self.fn_file_save_error_logger(local_logger, in_file_details, None)
+            except Exception as err:
+                self.fn_file_save_error_logger(local_logger, in_file_details, err)
 
     def fn_store_data_frame_to_file(self, local_logger, timmer, in_data_frame, in_file_details):
         timmer.start()
-        is_file_saved = False
-        given_format = self.fn_store_data_frame_to_file_validation(local_logger, in_file_details)
-        if given_format == 'csv':
-            self.fn_save_data_frame_to_csv(in_data_frame, in_file_details)
-            is_file_saved = True
-        elif given_format == 'excel':
-            in_data_frame.to_excel(excel_writer=in_file_details['name'],
-                                   engine='xlsxwriter',
-                                   freeze_panes=(1, 1),
-                                   verbose=True)
-            is_file_saved = True
-        elif given_format == 'pickle':
-            self.fn_save_data_frame_to_pickle(in_data_frame, in_file_details)
-            is_file_saved = True
-        if is_file_saved:
-            local_logger.info(self.lcl.gettext(
-                'Pandas Data Frame has just been saved to file "{file_name}", '
-                + 'considering {file_type} as file type')
-                              .replace('{file_name}', in_file_details['name'])
-                              .replace('{file_type}', in_file_details['format']))
+        self.fn_store_data_frame_to_file_validation(local_logger, in_file_details)
+        if 'format' in in_file_details:
+            self.fn_save_data_frame_to_csv(local_logger, in_data_frame, in_file_details)
+            self.fn_save_data_frame_to_excel(local_logger, in_data_frame, in_file_details)
+            self.fn_save_data_frame_to_pickle(local_logger, in_data_frame, in_file_details)
         timmer.stop()
 
     def fn_store_data_frame_to_file_validation(self, local_logger, in_file_details):
-        are_settings_ok = False
         if 'format' in in_file_details:
             implemented_file_formats = ['csv', 'excel', 'pickle']
             given_format = in_file_details['format'].lower()
-            if given_format in implemented_file_formats:
-                are_settings_ok = given_format
-            else:
+            if given_format not in implemented_file_formats:
                 local_logger.error(self.lcl.gettext(
-                        'File "format" attribute has a value of "{format_value}" '
-                        + 'which is not among currently implemented values: '
-                        + '"{implemented_file_formats}", therefore file saving is not possible')
+                    'File "format" attribute has a value of "{format_value}" '
+                    + 'which is not among currently implemented values: '
+                    + '"{implemented_file_formats}", therefore file saving is not possible')
                                    .replace('{format_value}', given_format)
                                    .replace('{implemented_file_formats}',
                                             '", "'.join(implemented_file_formats)))
@@ -93,4 +109,3 @@ class DataInputOutput:
             local_logger.error(self.lcl.gettext('File "format" attribute is mandatory '
                                                 + 'in the file setting, but missing, '
                                                 + 'therefore file saving is not possible'))
-        return are_settings_ok
