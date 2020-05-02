5eb869fb0eb6709931714c38a7980e78a8ef7114
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-05-02 21:21:50 +0300

    just code formatting

--- sources/extractor.py
+++ sources/extractor.py
@@ -20,21 +20,20 @@ if __name__ == '__main__':
     # initiate Logging sequence
     c_en.initiate_logger_and_timer()
     # reflect title and input parameters given values in the log
-    c_en.class_clam.listing_parameter_values(c_en.class_ln.logger, c_en.timer, 'Database Extractor',
-                                             c_en.config['input_options'][SCRIPT_NAME],
-                                             c_en.parameters)
+    c_en.class_clam.listing_parameter_values(
+        c_en.class_ln.logger, c_en.timer, 'Database Extractor',
+        c_en.config['input_options'][SCRIPT_NAME], c_en.parameters)
     # loading extracting sequence details
     c_en.load_extraction_sequence_and_dependencies()
     # validation of the extraction sequence file
-    if c_en.class_bnfe.validate_all_json_files(c_en.class_ln.logger, c_en.timer,
-                                               c_en.file_extract_sequence):
+    if c_en.class_bnfe.validate_all_json_files(
+            c_en.class_ln.logger, c_en.timer, c_en.file_extract_sequence):
         # cycling through the configurations
         for seq_idx, crt_sequence in enumerate(c_en.file_extract_sequence):
             can_proceed = \
-                c_en.class_bnfe.validate_all_json_files_current(c_en.class_ln.logger, c_en.timer,
-                                                                crt_sequence, seq_idx,
-                                                                c_en.source_systems,
-                                                                c_en.user_credentials)
+                c_en.class_bnfe.validate_all_json_files_current(
+                    c_en.class_ln.logger, c_en.timer, crt_sequence, seq_idx,
+                    c_en.source_systems, c_en.user_credentials)
             if c_en.class_bn.fn_evaluate_dict_values(can_proceed):
                 c_en.class_dbt.connect_to_database(c_en.class_ln.logger, c_en.timer,
                                                    c_en.class_bnfe.connection_details)
@@ -43,8 +42,8 @@ if __name__ == '__main__':
                     cursor = c_en.class_dbt.conn.cursor()
                     for crt_query in crt_sequence['queries']:
                         can_proceed_q = \
-                            c_en.class_bnfe.validate_extraction_query(c_en.class_ln.logger,
-                                                                      c_en.timer, crt_query)
+                            c_en.class_bnfe.validate_extraction_query(
+                                c_en.class_ln.logger, c_en.timer, crt_query)
                         if can_proceed_q:
                             the_query = c_en.load_query(crt_query)
                             for crt_session in crt_query['sessions']:
@@ -70,16 +69,16 @@ if __name__ == '__main__':
                                         'query': the_query,
                                         'session': crt_session,
                                     }
-                                    stats = c_en.extract_query_to_result_set(c_en.class_ln.logger,
-                                                                             cursor, dict_prepared)
+                                    stats = c_en.extract_query_to_result_set(
+                                        c_en.class_ln.logger, cursor, dict_prepared)
                                     if stats['rows_counted'] > 0:
                                         dict__child__parent__grand_parent = c_en.pack_three_levels(
                                             crt_session, crt_query, crt_sequence)
                                         rdf = c_en.result_set_into_data_frame(
                                             c_en.class_ln.logger, stats,
                                             dict__child__parent__grand_parent)
-                                        c_en.store_result_set_to_disk(c_en.class_ln.logger,
-                                                                      rdf, crt_session)
+                                        c_en.store_result_set_to_disk(
+                                            c_en.class_ln.logger, rdf, crt_session)
                         c_en.close_cursor(c_en.class_ln.logger, cursor)
                     c_en.close_connection(c_en.class_ln.logger)
     # just final message
