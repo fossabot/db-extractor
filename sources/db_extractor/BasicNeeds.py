723d3eb2c2d428da5ee7c9358b39736a4dfc3b03
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-06-07 05:17:32 +0300

    perfected log file input parameter check

--- sources/db_extractor/BasicNeeds.py
+++ sources/db_extractor/BasicNeeds.py
@@ -80,8 +80,10 @@ class BasicNeeds:
         return list_with_values
 
     def fn_check_inputs(self, input_parameters):
-        if input_parameters.output_log_file is not None:
+        if input_parameters.output_log_file not in (None, 'None'):
             # checking log folder first as there's all further messages will be stored
+            self.fn_timestamped_print(self.locale.gettext(
+                'Checking if provided folder for the log file is valid'))
             self.fn_validate_single_value(
                     os.path.dirname(input_parameters.output_log_file), 'folder')
 
